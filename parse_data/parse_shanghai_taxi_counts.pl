aff6cdadee5c039cfec1944511d9d1f39d1f5253
yichao0319 <yichao0319@gmail.com>
2015-11-09 10:58:45 +0800

    contact counts

--- parse_data/parse_shanghai_taxi_counts.pl
+++ parse_data/parse_shanghai_taxi_counts.pl
@@ -1,24 +1,22 @@
 #!/bin/perl
 
 ##########################################
-## Author: Yi-Chao Chen @ Huawei
+## Author: Yi-Chao Chen @ UT Austin
 ##
 ## - input:
 ##
 ## - output:
 ##
 ## - e.g.
-##   perl parse_rome_taxi_counts.pl 60 500
+##   
 ##
 ##########################################
 
 use strict;
 use POSIX;
 use List::Util qw(first max maxstr min minstr reduce shuffle sum);
-use Math::Trig qw(deg2rad pi great_circle_distance asin acos);
 # use lib "/u/yichao/utils/perl";
 # use lib "../utils";
-
 
 #############
 # Debug
@@ -32,18 +30,13 @@ my $DEBUG3 = 1; ## print output
 #############
 # Constants
 #############
-my $input_dir  = "./raw_rome_taxi";
-my $output_dir = "./processed_rome_taxi";
-my $filename = "taxi_february";
-# my $filename = "short";
-
-
-
+my $input_dir  = "./raw_shanghai_taxi";
+my $output_dir = "./processed_shanghai_taxi";
+# my $filename = "bus";
 
 #############
 # Variables
 #############
-my $cmd;
 my %trace;
 my %times;
 
@@ -64,47 +57,85 @@ my $range = $ARGV[1] + 0;
 #############
 
 #############
-## uncompress file
+## decompress
 #############
-# $cmd = "gunzip $input_dir/*gz";
+# print "Decompress\n" if($DEBUG2);
+
+# my $cmd = "gunzip $input_dir/*gz";
 # `$cmd`;
 
 
 #############
 ## read directory
 #############
-print "Read directory\n" if($DEBUG2);
+opendir(my $dh, $input_dir) || die $!;
+while(readdir $dh) {
+    chomp;
+    if($_ =~ /(Taxi.*)/) {
+        my $filename = $1;
+        print "  $filename\n" if($DEBUG3);
 
-open FH, "$input_dir/$filename.txt" or die $!;
-while (<FH>) {
-    # print $_;
+        #############
+        ## Read Data
+        #############
+        # print "Read Data\n" if($DEBUG2);
+        
+        open FH, "$input_dir/$filename" or die $!;
+        while(<FH>) {
+            print $_ if($DEBUG3);
+            chomp;
+            my @data = split(/,/, $_);
+            my $car = $data[0];
+            my $lng = $data[2] + 0;
+            my $lat = $data[3] + 0;
 
-    if($_ =~ /(\d+);(\d+)-(\d+)-(\d+)\s+(\d+):(\d+):(\d+\.*\d*)\+(\d+);POINT\((-*\d+\.\d+)\s+(-*\d+\.\d+)\)/) {
-        my $carid = $1+0;
-        my $year = $2+0;
-        my $mon = $3+0;
-        my $day = $4+0;
-        my $hour = $5+0;
-        my $min = $6+0;
-        # my $sec = $7*(10**$8);
-        my $sec = round($7*(10**$8));
-        my $locx = $9+0;
-        my $locy = $10+0;
-        # print "$year-$mon-$day $hour:$min:$sec: car=$carid ($locx, $locy)\n";
+            my $time = 0;
+            if($data[1] =~ /(\d+)-(\d+)-(\d+)\s+(\d+):(\d+):(\d+)/) {
+                my $year = $1 + 0;
+                my $mon = $2 + 0;
+                my $day = $3 + 0;
+                my $hour = $4 + 0;
+                my $min = $5 + 0;
+                my $sec = $6 + 0;
+                $time = (((get_month_cumdays($mon-1) + $day)*24 + $hour)*60 + $min)*60 + $sec;
 
-        my $time = (((get_month_cumdays($mon-1) + $day)*24 + $hour)*60 + $min)*60 + $sec;
-        $times{$time} = 1;
+                $times{$time} = 1;
+            }
 
-        $trace{$carid}{TIME}{$time}{X} = $locx;
-        $trace{$carid}{TIME}{$time}{Y} = $locy;
+            print "  car $car [$lat, $lng] at $time\n" if($DEBUG3);
+            # $trace{$car}{$time}{X} = $lat;
+            # $trace{$car}{$time}{Y} = $lng;
+            $trace{$car}{TIME}{$time}{X} = $lat;
+            $trace{$car}{TIME}{$time}{Y} = $lng;
+
+        }
+        close FH;
     }
-    else {
-        print $_;
-        print "ERROR: Wrong Format\n";
-    }
-
 }
-close FH;
+closedir $dh;
+
+
+# print "  #car = ".(scalar keys %trace)."\n";
+# foreach my $this_car (sort {$a <=> $b} keys %trace) {
+#     next if (scalar (keys %{$trace{$this_car}}) < 1000);
+
+#     open FH, "> $output_dir/cars/$this_car.txt" or die $!;
+#     foreach my $this_time (sort {$a <=> $b} keys %{$trace{$this_car}}) {
+#         print FH "$this_time, ".$trace{$this_car}{$this_time}{X}.", ".$trace{$this_car}{$this_time}{Y}."\n";
+#     }
+#     close FH;
+
+#     my @tmp = sort {$a <=> $b} keys %{$trace{$this_car}};
+#     print "  car $this_car: #loc=".(keys %{$trace{$this_car}}).", time=".($tmp[@tmp-1] - $tmp[0])."\n";
+# }
+# return
+
+# my @tmp = sort {$a <=> $b} (keys %times);
+# print "  overall time = ".($tmp[@tmp-1] - $tmp[0])."\n";
+
+# open FH, "> $output_dir/times.txt" or die $!;
+# print FH join("\n", sort {$a <=> $b} (keys %times))."\n";
+# close FH;
 
 
 #############
@@ -159,41 +190,6 @@ foreach my $this_car (keys %trace) {
     }
 }
 
-# foreach my $this_car (keys %trace) {
-#     my $tidx = 0;
-#     my $pre_time = -1;
-#     my $prex = 0;
-#     my $prey = 0;
-
-#     my @old_times = sort {$a <=> $b} keys %{$trace{$this_car}{TIME}};
-#     foreach my $this_time (@old_times) {
-#         my $thisx = $trace{$this_car}{TIME}{$this_time}{X};
-#         my $thisy = $trace{$this_car}{TIME}{$this_time}{Y};
-#         print "car$this_car: car=$this_time [$thisx,$thisy], all=".$times[$tidx]."\n";
-#         last if($tidx == @old_times);
-
-#         while($this_time > $times[$tidx]) {
-#             my $interp_time = $times[$tidx];
-
-#             my ($interpx, $interpy) = interp($pre_time, $prex, $prey, $this_time, $thisx, $thisy, $interp_time);
-#             $trace{$this_car}{TIME}{$interp_time}{X} = $interpx;
-#             $trace{$this_car}{TIME}{$interp_time}{Y} = $interpy;
-#             print "  idx$tidx: interp time=$interp_time [$interpx, $interpy]\n";
-            
-#             $tidx ++;
-#         }
-#         if($this_time != $times[$tidx]) {
-#             print "$tidx: $this_time, ".$times[$tidx]."\n";
-#             die "ERROR: time diff";
-#         }
-
-#         $pre_time = $this_time;
-#         $prex = $thisx;
-#         $prey = $thisy;
-#         $tidx ++;
-#     }
-# }
-
 
 #############
 ## Find contact counts
@@ -240,20 +236,6 @@ foreach my $this_time (@times) {
     }
 }
 
-# print "#car = ".(scalar keys %trace)."\n";
-# foreach my $this_car (sort {$a <=> $b} keys %trace) {
-#     open FH, "> $output_dir/cars/$this_car.txt" or die $!;
-#     foreach my $i (0 .. @{$trace{$this_car}{TIME}}-1) {
-#         print FH "".$trace{$this_car}{TIME}[$i].", ".$trace{$this_car}{X}[$i].", ".$trace{$this_car}{Y}[$i]."\n";
-#     }
-#     close FH;
-#     print "  car $this_car: #loc=".(@{$trace{$this_car}{TIME}})."\n";
-# }
-# # return
-
-# open FH, "> $output_dir/times.txt" or die $!;
-# print FH join("\n", sort(keys %times))."\n";
-# close FH;
 
 open FH, "> $output_dir/counts.$itvl.$range.txt" or die $!;
 print FH join("\n", sort(@counts))."\n";
@@ -261,69 +243,14 @@ close FH;
 
 
 #############
-## compress file
+## compress
 #############
-# $cmd = "gzip $input_dir/taxi_february*";
+# print "Compress\n" if($DEBUG2);
+
+# my $cmd = "gzip $input_dir/Taxi* taxi.gzip";
 # `$cmd`;
 
 
-
-sub LawCosines {
-    my ($lat1, $long1, $lat2, $long2) = @_;
-    my $r=6367000;
-
-    my $dist = acos(sin(deg2rad($lat1))*
-                sin(deg2rad($lat2))+
-                cos(deg2rad($lat1))*
-                cos(deg2rad($lat2))*
-                cos(deg2rad($long2)- deg2rad($long1))) 
-                * $r;
-    return $dist;
-
-}
-
-sub FlatEarth {
-    my ($lat1, $long1, $lat2, $long2) = @_;
-    my $r=6367000;
-
-    my $a = (pi/2)- deg2rad($lat1);               
-    my $b = (pi/2)- deg2rad($lat2);
-    my $c = sqrt($a**2 + $b**2 - 2 * $a *$b
-            *cos(deg2rad($long2)-deg2rad($long1)));
-    my $dist = $c * $r;
-
-    return $dist;
-
-}
-
-sub Haversine {
-    my ($lat1, $long1, $lat2, $long2) = @_;
-    my $r=6367000;
-
-               
-    my $dlong = deg2rad($long1) - deg2rad($long2);
-    my $dlat  = deg2rad($lat1) - deg2rad($lat2);
-
-    my $a = sin($dlat/2)**2 +cos(deg2rad($lat1)) 
-                    * cos(deg2rad($lat2))
-                    * sin($dlong/2)**2;
-    my $c = 2 * (asin(sqrt($a)));
-    my $dist = $r * $c;               
-
-
-    return $dist;
-
-}
-
-sub GreatCircle {
-    my ($lat1, $long1, $lat2, $long2) = @_;
-    my $r=6367000;
-    my @zip1 = (deg2rad($long1), deg2rad(90-$lat1));
-    my @zip2 = (deg2rad($long2), deg2rad(90-$lat2));
-    my $dist = great_circle_distance(@zip1, @zip2,$r);
-                     
-    return $dist;
-}
 
 
 
@@ -341,6 +268,18 @@ sub get_month_cumdays {
     
     return $days[$mon-1];
 }
+
+
+sub GreatCircle {
+    my ($lat1, $long1, $lat2, $long2) = @_;
+    my $r=6367000;
+    my @zip1 = (deg2rad($long1), deg2rad(90-$lat1));
+    my @zip2 = (deg2rad($long2), deg2rad(90-$lat2));
+    my $dist = great_circle_distance(@zip1, @zip2,$r);
+                     
+    return $dist;
+}
+
 
 sub interp {
     my ($pre_time, $prex, $prey, $this_time, $thisx, $thisy, $interp_time) = @_;
